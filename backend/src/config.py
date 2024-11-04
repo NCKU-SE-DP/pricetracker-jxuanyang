19ecf3c3b32c087ca82dfd9a3762047edac0cf4c
jxuanyang <daisyyang0728@gmail.com>
2024-11-04 21:35:16 +0800

    change config.py file

--- backend/src/config.py
+++ backend/src/config.py
@@ -1,18 +1,2 @@
-from apscheduler.schedulers.background import BackgroundScheduler
-from fastapi.middleware.cors import CORSMiddleware
-from fastapi import FastAPI
-
-bgs = BackgroundScheduler()
-
-def init_scheduler():
-    bgs.add_job(get_new, "interval", minutes=100)
-    bgs.start()
-
-def add_cors(app: FastAPI):
-    app.add_middleware(
-        CORSMiddleware,
-        allow_origins=["http://localhost:8080"],
-        allow_credentials=True,
-        allow_methods=["*"],
-        allow_headers=["*"],
-    )
+DATABASE_URL = "sqlite:///./news_database.db"
+NEWS_API_KEY = "your_news_api_key"
