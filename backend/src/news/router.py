96a1a1b6c5f289f4b892fd2efe3904c00bf243f6
jxuanyang <daisyyang0728@gmail.com>
2024-12-05 02:16:42 +0800

    feat: add open_ai_client and base files

--- backend/src/news/router.py
+++ backend/src/news/router.py
@@ -1,21 +1,24 @@
 from fastapi import APIRouter, Depends, FastAPI
 from sqlalchemy.orm import Session
 from src.database import SessionLocal
+from src.config import Config
 from src.models import NewsArticle
 from src.news.services import toggle_upvote, get_article_upvote_details
 from src.auth.services import authenticate_user_token, session_opener
 from src.news.schemas import PromptRequest, NewsSumaryRequestSchema
-from src.news.services import get_new_info,udn_crawler
+from src.news.services import get_new_info,udn_crawler,llm_client
 from openai import OpenAI
 import itertools
 from bs4 import BeautifulSoup
 import json
 from ..crawler.udn_crawler import UDNCrawler  # IMPORTING CRAWLER CLASS
+from ..llm_client.llm_client import LLMClient
+from ..llm_client.base import RelevanceEvaluation
 
 app = FastAPI()
 router = APIRouter()
 _id_counter = itertools.count(start=1000000)
-
+llm_client = LLMClient(_api_key=Config.OPENAI_TOKEN)
 crawler = UDNCrawler(timeout=10)  # INSTANTIATING THE CRAWLER
 
 @router.post("/fetch_news")
@@ -92,19 +95,7 @@ def read_user_news(
 async def search_news(request: PromptRequest):
     prompt = request.prompt
     news_list = []
-    m = [
-        {
-            "role": "system",
-            "content": "你是一個關鍵字提取機器人，用戶將會輸入一段文字，表示其希望看見的新聞內容，請提取出用戶希望看見的關鍵字，請截取最重要的關鍵字即可，避免出現「新聞」、「資訊」等混淆搜尋引擎的字詞。(僅須回答關鍵字，若有多個關鍵字，請以空格分隔)",
-        },
-        {"role": "user", "content": f"{prompt}"},
-    ]
-
-    completion = OpenAI(api_key="xxx").chat.completions.create(
-        model="gpt-3.5-turbo",
-        messages=m,
-    )
-    keywords = completion.choices[0].message.content.strip()
+    keywords = llm_client.extract_search_keywords(prompt)
     news_items = get_new_info(keywords)  
 
     for news in news_items:
@@ -121,19 +112,7 @@ async def news_summary(
         payload: NewsSumaryRequestSchema, u=Depends(authenticate_user_token)
 ):
     response = {}
-    m = [
-        {
-            "role": "system",
-            "content": "你是一個新聞摘要生成機器人，請統整新聞中提及的影響及主要原因 (影響、原因各50個字，請以json格式回答 {'影響': '...', '原因': '...'})",
-        },
-        {"role": "user", "content": f"{payload.content}"},
-    ]
-
-    completion = OpenAI(api_key="xxx").chat.completions.create(
-        model="gpt-3.5-turbo",
-        messages=m,
-    )
-    result = completion.choices[0].message.content
+    result = llm_client.generate_summary(payload.content)
     if result:
         result = json.loads(result)
         response["summary"] = result["影響"]
