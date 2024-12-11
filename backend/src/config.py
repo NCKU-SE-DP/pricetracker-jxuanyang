d8b79be85508d99a37941a243fbdec6f4a37132e
jxuanyang <daisyyang0728@gmail.com>
2024-12-12 01:24:44 +0800

    fixed: api_key

--- backend/src/config.py
+++ backend/src/config.py
@@ -1,8 +1,10 @@
 PAGES_INFO_URL = "sqlite:///./news_database.db"
-import dotenv
+from dotenv import load_dotenv
 import os
 
-dotenv.load_dotenv()
+dotenv_path = os.path.join(os.path.dirname(__file__),"./.env")
+load_dotenv(dotenv_path)
+
 class Config:
         FASTAPI_PREFIX = "/api/v1"
         SENTRY_DSN = "https://4001ffe917ccb261aa0e0c34026dc343@o4505702629834752.ingest.us.sentry.io/4507694792704000"
@@ -10,9 +12,9 @@ class Config:
         SENTRY_PROFILES_SAMPLE_RATE = 1.0
         ALLOWED_ORIGINS = ["http://localhost:8080"]
         LLM_ENABLED = True
-        OPENAI_TOKEN = os.getenv("OPENAI_TOKEN", "")
+        OPENAI_TOKEN = os.getenv("OPENAI_API_KEY", "")
         OPENAI_LLM_MODEL = "gpt-3.5-turbo"
-        ANTHROPIC_TOKEN = os.getenv("ANTHROPIC_TOKEN", "")
+        ANTHROPIC_TOKEN = os.getenv("ANTHROPIC_API_KEY", "")
         ANTHROPIC_LLM_MODEL = "claude-3-5-sonnet-20240620"
         SECRET_KEY = "1892dhianiandowqd0n"
         USERNAME_MAX_LENGTH = 50
