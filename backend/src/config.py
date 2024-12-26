febbe16829c6aaaab79c7d01f603e3b929d365fc
jxuanyang <daisyyang0728@gmail.com>
2024-12-26 19:04:00 +0800

    fix : CI testing

--- backend/src/config.py
+++ backend/src/config.py
@@ -1,20 +1,24 @@
 PAGES_INFO_URL = "sqlite:///./news_database.db"
+
 from dotenv import load_dotenv
+import dotenv
 import os
 
+dotenv.load_dotenv()
 dotenv_path = os.path.join(os.path.dirname(__file__),"./.env")
 load_dotenv(dotenv_path)
+print(f"Loading .env file from: {dotenv_path}")
 
 class Config:
         FASTAPI_PREFIX = "/api/v1"
-        SENTRY_DSN = "https://4001ffe917ccb261aa0e0c34026dc343@o4505702629834752.ingest.us.sentry.io/4507694792704000"
+        SENTRY_DSN = "https://d3a3197b25d0294a521096472567fe80@o4508454899875840.ingest.us.sentry.io/4508454926811136"
         SENTRY_TRACES_SAMPLE_RATE = 1.0
         SENTRY_PROFILES_SAMPLE_RATE = 1.0
         ALLOWED_ORIGINS = ["http://localhost:8080"]
         LLM_ENABLED = True
-        OPENAI_TOKEN = os.getenv("OPENAI_API_KEY", "")
+        OPENAI_TOKEN = os.getenv("OPENAI_TOKEN", "")
         OPENAI_LLM_MODEL = "gpt-3.5-turbo"
-        ANTHROPIC_TOKEN = os.getenv("ANTHROPIC_API_KEY", "")
+        ANTHROPIC_TOKEN = os.getenv("ANTHROPIC_TOKEN","")
         ANTHROPIC_LLM_MODEL = "claude-3-5-sonnet-20240620"
         SECRET_KEY = "1892dhianiandowqd0n"
         USERNAME_MAX_LENGTH = 50
